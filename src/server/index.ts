c914605a6a292a5b265f37f2d13768c83030fb1c
Erin Dachtler <erin@erindachtler.me>
2020-09-22 21:24:18 -0700

    server working

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,11 @@
 import express from 'express'
-import expressJWT from 'express-jwt'
 import webpush from 'web-push'
 import createStore from './store.js'
 import addRoutes from './routes/index.js'
 
 import 'dotenv/config.js'
-const { JWT_SECRET, VAPID_CONTACT, VAPID_PUBLIC_KEY, VAPID_PRIVATE_KEY} = process.env
-if(!JWT_SECRET || !VAPID_CONTACT || !VAPID_PUBLIC_KEY || !VAPID_PRIVATE_KEY) {
+const { VAPID_CONTACT, VAPID_PUBLIC_KEY, VAPID_PRIVATE_KEY} = process.env
+if(!VAPID_CONTACT || !VAPID_PUBLIC_KEY || !VAPID_PRIVATE_KEY) {
 	throw 'Make a .env file! See README.md for details'
 }
 
@@ -16,22 +15,17 @@ webpush.setVapidDetails(
 	VAPID_PRIVATE_KEY
 )
 
-async function main(jwtSecret: string) {
+async function main() {
 	const app = express()
 
 	const store = await createStore()
 
-	app.use(express.static('dist/public', {extensions: ['html']}))
-
-	app.use(expressJWT({
-		secret: jwtSecret,
-		credentialsRequired: false,
-		algorithms: ['HS256']
-	}))
+	app.use(express.static('src/server/static', { extensions: ['html'] }))
+	app.use(express.static('dist/client'))
 
 	addRoutes(app, store)
 
 	app.listen(80, () => console.log('Listening on 80'))
 }
 
-main(JWT_SECRET)
+main()
